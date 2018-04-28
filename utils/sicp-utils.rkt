f48a980fb051b0bb62a7dd6613be2b3c061fb1c0
jonmoore <jonathanmoorephd@gmail.com>
2018-04-28 18:03:46 -0400

    Add a few helpers.  Avoid racket defns in tests.

--- utils/sicp-utils.rkt
+++ utils/sicp-utils.rkt
@@ -1,18 +1,52 @@
 #lang sicp
 
-(#%provide begin-example nth !=)
+(#%require (only racket module+) rackunit)
 
 (define (disp text)
   (display text (current-output-port)))
 
+(#%provide begin-example)
 (define (begin-example text)
   (newline)
   (disp "Example ") (disp text) (newline)
   (disp "-----------------------------------------------------------")
-  (newline)
-  )
+  (newline))
 
+(#%provide nth)
 (define nth list-ref)
 
+(#%provide !=)
 (define (!= a b)
   (not (= a b)))
+
+(#%provide make-timer)
+(define (make-timer)
+  (define (timer-from start)
+    (lambda ()
+      (/ (- (runtime) start) 1e6)))
+  (timer-from (runtime)))
+
+;; https://stackoverflow.com/a/12610253 says that The Little Schemer
+;; uses this.
+(#%provide atom?)
+(define (atom? x)
+  (and (not (pair? x))
+       (not (null? x))))
+(module+ test
+  (test-case "empty list"
+             (check-pred atom? #f)
+             (check-true (atom? 3))
+             (check-true (atom? #f))
+             (check-false (atom? '()))
+             (check-false (atom? '(1)))
+             (check-false (atom? '(())))
+             (check-false (atom? '((1))))
+             (check-false (atom? '((1 2))))
+             (check-false (atom? '((1) 2)))
+             ))
+
+
+(#%provide constant)
+(define (constant x)
+  (lambda () x))
+
