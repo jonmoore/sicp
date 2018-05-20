06d8b10fc6a2f19c73cd2fd2222d3d9f649d29de
jonmoore <jonathanmoorephd@gmail.com>
2018-05-20 13:28:20 -0400

    Delete obsolete work-in-progress files

--- wip/test-geiser-printing.rkt
+++
@@ -1,9 +0,0 @@
-#lang sicp
-
-(define slist (list 3 4))
-
-;; (require (only-in racket/base print-as-expression))
-;; (require (only-in racket/base print-as-expression))
-
-;;  (print-as-expression #f)
-
