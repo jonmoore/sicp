934458275acb15826ad90c112913255e2cc83b50
jonmoore <jonathanmoorephd@gmail.com>
2018-05-20 23:01:17 -0400

    generalize and #%provide search-int

--- chap1/ex1.37.rkt
+++ chap1/ex1.37.rkt
@@ -4,27 +4,6 @@
 
 ;; Exercise 1.37:
 
-;; An infinite continued fraction is an expression of the form
-;; f=N1D1+N2D2+N3D3+….
-
-;; As an example, one can show that the infinite continued fraction
-;; expansion with the Ni and the Di all equal to 1 produces 1/φ, where φ
-;; is the golden ratio (described in 1.2.2). One way to approximate an
-;; infinite continued fraction is to truncate the expansion after a given
-;; number of terms. Such a truncation—a so-called finite continued
-;; fraction k-term finite continued fraction—has the form N1D1+N2⋱+NkDk.
-
-;; Suppose that n and d are procedures of one argument (the term index i)
-;; that return the Ni and Di of the terms of the continued
-;; fraction. Define a procedure cont-frac such that evaluating (cont-frac
-;; n d k) computes the value of the k-term finite continued
-;; fraction. Check your procedure by approximating 1/φ using
-
-;; (cont-frac (lambda (i) 1.0) (lambda (i) 1.0) k)
-
-;; for successive values of k. How large must you make k in order to get
-;; an approximation that is accurate to 4 decimal places?
-
 (define (cont-frac-combiner n d)
   (lambda (i accum)(/ (n i) (+ (d i) accum))))
 
@@ -43,11 +22,12 @@
   (displn "Recursive calculation of 1/phi with cont-frac, 10 terms")
   (displn  ((phi-calc cont-frac) 10)))
 
+(#%provide search-int)
 (define (search-int f a b)
   ;; Search for an adjacent pair of integers in [a,b] for which
-  ;; (compose positive? f) has opposite signs
+  ;; f has different values
   (define (equiv-int x y)
-    (equal? (positive? (f x)) (positive? (f y))))
+    (equal? (f x) (f y)))
   (define (iter a b)
     (let ((midpoint
            (+ a (quotient (- b a) 2))))
@@ -59,7 +39,7 @@
        (else
         (iter a midpoint)))))
   (if (equiv-int a b)
-      (error "(compose positive? f) must differ at a and b"))
+      (error "f must differ at a and b"))
   (iter a b))
 
 (define invphi (/ (dec (sqrt 5)) 2.0))
@@ -72,10 +52,12 @@
   (displn (map (lambda (k) (list k (invk-error k)))
                (iota 10 2)))
   (displn "Number of iterations needed for error < 1e-4")
+
+  (define (invk-error-in-target? k)
+    (positive? (- (invk-error k) 1e-4)))
   (displn
    (cadr
-    (search-int (lambda (k) (- (invk-error k) 1e-4)) 2 32)))
-  )
+    (search-int invk-error-in-target? 2 32))))
 
 (define (cont-frac-iter n d k)
   (define (iter i accum)
