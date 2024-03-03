ba8f5980c988bde6b107e5dd2768fcc0b69b8e7b
Jonathan Moore <jonathanmoorephd@gmail.com>
2024-03-03 10:17:35 -0500

    rename running with errortrace as r-run

--- setup.bat
+++ setup.bat
@@ -6,6 +6,6 @@ set "PATH=c:\Program Files\Racket;%PATH%"
 @echo on
 doskey rkt=racket --require $*
 doskey r-enter=racket --repl --eval "(enter! \"$1\")"
-doskey r-trace=racket --lib errortrace --require $*
+doskey r-run=racket --lib errortrace --require $*
 doskey r-sicp=racket --eval "(#%%require racket/help racket/trace)"  --lib sicp --repl $*
 
